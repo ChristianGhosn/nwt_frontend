be510ef5b11dc07d4fab11ad685820567333a839
ChristianGhosn <christianjghosn@gmail.com>
2025-07-20 16:16:44 +1000

    Repositioned hot toast to top center

--- src/main.jsx
+++ src/main.jsx
@@ -17,7 +17,7 @@ createRoot(document.getElementById("root")).render(
     }}
   >
     <Toaster
-      position="top-right"
+      position="top"
       reverseOrder={false}
       toastOptions={{
         duration: 3000,
